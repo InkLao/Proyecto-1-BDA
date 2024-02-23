6865b8fb061f38b14124ae22059231af53857128
ang3lfco <149128034+ang3lfco@users.noreply.github.com>
2024-02-22 17:04:27 -0700

    (M/Menu-IS-CC) (C/CC) / Conversiones
    actualizacion y cambios.
    implementacion de la clase Conversiones.

---
+++ Proyecto-1-BDA-master/src/main/java/frm/Conversiones.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package frm;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Martinez
+ */
+public class Conversiones {
+    public List<List<Object>> obtenerCuentasCliente(String idCliente) {
+        List<List<Object>> cuentas = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/banco", "root", "41502Mar")) {
+            String sql = "SELECT * FROM cuenta WHERE idCliente = ?";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setString(1, idCliente);
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    while (resultSet.next()) {
+                        List<Object> cuenta = new ArrayList<>();
+                        cuenta.add(resultSet.getLong("numCuenta"));
+                        cuenta.add(resultSet.getDouble("saldo"));
+                        cuentas.add(cuenta);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al cargar las cuentas: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return cuentas;
+    }
+}
